e4e13cd443720ff384956f38be9802dab9344cbc
Kamran Aliyev <aliyevcameron@gmail.com>
2026-08-09 21:44:20 -0400

    Create database tables

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travel_social.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+from models import User, TravelPost
+
+
 @app.route("/")
 def home():
     return "Hello World!"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
