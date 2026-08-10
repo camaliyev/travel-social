9e8fa457a98fdd19f70175eebd4051a598894210
Kamran Aliyev <aliyevcameron@gmail.com>
2026-08-09 21:29:55 -0400

    Create TravelPost model and relationships

--- models.py
+++ models.py
@@ -1,7 +1,20 @@
 from app import db
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False, index=True)
     email = db.Column(db.String(120), unique=True, nullable=False, index=True)
     password_hash = db.Column(db.String(200), nullable=False)
+
+    posts = db.relationship('TravelPost', backref='author', lazy=True)
+
+
+class TravelPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    country = db.Column(db.String(50), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
