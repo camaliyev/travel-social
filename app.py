4747b2b443065efc8e6a2955869a838304680054
Kamran Aliyev <cam@Mac.lan>
2026-08-09 20:53:40 -0400

    Configure SQLAlchemy database

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
-
-print("App is starting...")
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travel_social.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
     return "Hello World!"
 
 if __name__ == "__main__":
-    print("Running Flask...")
     app.run(debug=True)
