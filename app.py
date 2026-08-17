c7cdc7b68ee2c4ff1c7a98691f5e4f3ea26fbbe0
Kamran Aliyev <aliyevcameron@gmail.com>
2026-08-17 19:34:47 -0400

    Add custom 404 and 403 error pages

--- app.py
+++ app.py
@@ -35,5 +35,13 @@ def home():
     )
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+@app.errorhandler(403)
+def forbidden(error):
+    return render_template("403.html"), 403
+
 if __name__ == "__main__":
     app.run(debug=True)
